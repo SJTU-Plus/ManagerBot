3a039b5eeda89c57649e4074957488592b834e90
dujiajun <836737136@qq.com>
2020-06-05 11:42:30 +0800

    expose wsgi app

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@ import nonebot
 
 import config
 
+nonebot.init(config)
+nonebot.load_plugin(
+    'plugins.group_manager')
+bot = nonebot.get_bot()
+app = bot.asgi
+
 if __name__ == '__main__':
-    nonebot.init(config)
-    nonebot.load_plugin(
-        'plugins.group_manager')
     nonebot.run()
